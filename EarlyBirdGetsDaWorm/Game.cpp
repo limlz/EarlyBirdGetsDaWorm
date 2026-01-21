a68c3d7254ac4504597ddb883fbc9639df906b84
limlz <62799391+limlz@users.noreply.github.com>
2026-01-21 21:58:57 +0800

    macro move

--- EarlyBirdGetsDaWorm/Game.cpp
+++ EarlyBirdGetsDaWorm/Game.cpp
@@ -1,16 +1,6 @@
 #include "pch.hpp"
 
 // --- MACROS --- //
-// Colors
-#define COLOR_WHITE         0xFFFFFFFF
-#define COLOR_BLACK         0x000000FF
-#define COLOR_NIGHT_BLUE    0x191970FF
-#define COLOR_DOOR_BROWN    0x8B4513FF
-#define COLOR_LIFT_GREY     0x696969FF
-#define COLOR_LIFT_BG       0x2F4F4FFF
-#define COLOR_LIFT_CONSOLE  0xC0C0C0FF
-#define COLOR_LIFT_BUTTON   0xFFD700FF
-
 // Dimensions & Settings
 #define PLAYER_SPEED        1500.0f
 #define DOOR_WIDTH          150.0f
--- EarlyBirdGetsDaWorm/pch.hpp
+++ EarlyBirdGetsDaWorm/pch.hpp
@@ -17,5 +17,16 @@
 #include "game.hpp"
 #include "quit.hpp"
 
+// --- MACROS --- //
+// Colors
+#define COLOR_WHITE         0xFFFFFFFF
+#define COLOR_BLACK         0x000000FF
+#define COLOR_NIGHT_BLUE    0x191970FF
+#define COLOR_DOOR_BROWN    0x8B4513FF
+#define COLOR_LIFT_GREY     0x696969FF
+#define COLOR_LIFT_BG       0x2F4F4FFF
+#define COLOR_LIFT_CONSOLE  0xC0C0C0FF
+#define COLOR_LIFT_BUTTON   0xFFD700FF
+
 const int SCREEN_W = 1600;
 const int SCREEN_H = 900;
