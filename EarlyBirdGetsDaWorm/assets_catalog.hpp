9f958cbef75c300e634a3e3c367774f0061bbcb3
limlz <62799391+limlz@users.noreply.github.com>
2026-02-23 11:00:16 +0800

    Consolidate asset constants and fix unload leaks

---
+++ EarlyBirdGetsDaWorm/assets_catalog.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Assets
+{
+    namespace Fonts
+    {
+        inline constexpr const char* Buggy = "Assets/buggy-font.ttf";
+        inline constexpr const char* BuggyInFontsDir = "Assets/Fonts/buggy-font.ttf";
+        inline constexpr const char* DigitalClock = "Assets/Fonts/DS-DIGI.ttf";
+    }
+}
+
