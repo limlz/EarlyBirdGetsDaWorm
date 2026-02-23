1684a4b6c7763784e6a6e04a006ec80ffa236d7f
limlz <62799391+limlz@users.noreply.github.com>
2026-02-23 11:08:53 +0800

    fixing inline issues

--- EarlyBirdGetsDaWorm/assets_catalog.hpp
+++ EarlyBirdGetsDaWorm/assets_catalog.hpp
@@ -4,9 +4,9 @@ namespace Assets
 {
     namespace Fonts
     {
-        inline constexpr const char* Buggy = "Assets/buggy-font.ttf";
-        inline constexpr const char* BuggyInFontsDir = "Assets/Fonts/buggy-font.ttf";
-        inline constexpr const char* DigitalClock = "Assets/Fonts/DS-DIGI.ttf";
+        constexpr const char* Buggy = "Assets/buggy-font.ttf";
+        constexpr const char* BuggyInFontsDir = "Assets/Fonts/buggy-font.ttf";
+        constexpr const char* DigitalClock = "Assets/Fonts/DS-DIGI.ttf";
     }
 }
 
